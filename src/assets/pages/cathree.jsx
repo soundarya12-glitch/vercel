8a0a67d043041e1c4ea8aefbf9605fee836bee36
Soundarya <soundarya2300@gmail.com>
2025-09-22 11:29:46 +0530

    updatedddddddd code

--- src/assets/pages/cathree.jsx
+++ src/assets/pages/cathree.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import React from 'react';
-export default function Catwo() {
+export default function Cathree() {
   const [count, setCount] = useState(1);
 
   return (
